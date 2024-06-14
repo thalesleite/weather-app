19e2b8bb93cd6979d32029a75e867786a58852f5
thalesleite <thalesaleite@gmail.com>
2024-06-14 15:29:17 +0100

    add geocoding api to get the lat and lon when type city or country

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,11 @@ import "../css/styles.css"
 
 console.log("Weather App running!!!")
 
-const lat = "53.3149033"
-const lon = "-6.3143915"
 const apiKey = "abdecbd2a4a4083f2619b71a19fec61f"
+
+const codes = await getGeocoding()
+
+const { lat, lon } = codes
 
 fetch(
   `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
@@ -13,8 +15,18 @@ fetch(
     return response.json()
   })
   .then((response) => {
-    console.log("response: ", response)
+    const { weather } = response
+    console.log("weather: ", weather)
   })
   .catch((error) => {
     console.log("Error: ", error)
   })
+
+async function getGeocoding() {
+  const name = "Dublin"
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=1&appid=${apiKey}`
+  )
+  const codes = await response.json()
+  return codes[0]
+}
