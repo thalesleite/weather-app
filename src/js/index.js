e0675630b6be023da4a97fb4c6e1fc70a2b60b0a
thalesleite <thalesaleite@gmail.com>
2024-06-14 15:16:57 +0100

    call weather api and test return

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,20 @@
 import "../css/styles.css"
 
 console.log("Weather App running!!!")
+
+const lat = "53.3149033"
+const lon = "-6.3143915"
+const apiKey = "abdecbd2a4a4083f2619b71a19fec61f"
+
+fetch(
+  `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+)
+  .then((response) => {
+    return response.json()
+  })
+  .then((response) => {
+    console.log("response: ", response)
+  })
+  .catch((error) => {
+    console.log("Error: ", error)
+  })
